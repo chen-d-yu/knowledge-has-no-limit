8dd07d0e4c408f7c86636cca7b5abbdf7e37c06a
zuoyou <cdy1998cdy@163.com>
2023-02-06 23:23:49 +0800

    循环

---
+++ packages/system-learning/3-js/02_循环/01_while循环.cjs
@@ -0,0 +1,35 @@
+// let i = 0;
+// while (i < 5) {
+//   console.log("hello world");
+//   i++;
+// }
+
+// let i = 99;
+// let result = 0;
+// while (i >= 0) {
+//   result += i;
+//   i--;
+// }
+
+// let i = 99;
+// let result = 0;
+// while (i >= 0) {
+//   if (i % 2 === 0) {
+//     result += i;
+//   }
+//   i--;
+// }
+// console.log("result", result);
+// var cars = ["1", "2", "3"];
+// var i = 0,
+//   len = cars.length;
+// for (; ; i++) {
+//   if (i >= len) {
+//     break;
+//   }
+//   console.log(cars[i]);
+// }
+let result = "❗";
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < 5; j++) {}
+}
