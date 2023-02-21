2c794351fae83cb9e64fe2726213fa267ccbf736
zuoyou <cdy1998cdy@163.com>
2023-02-21 23:35:12 +0800

    123

--- packages/sharkbee-chinese-colors-react/src/App.tsx
+++ packages/sharkbee-chinese-colors-react/src/App.tsx
@@ -1,10 +1,9 @@
 import Home from './pages/Home';
 import './App.less';
+import {FC} from 'react';
 
 function App() {
-  return (
-      <Home />
-  );
+  return <Home />;
 }
 
 export default App;
