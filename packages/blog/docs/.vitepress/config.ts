8170677aea17df58d139314a94de424138cd5718
zuoyou <cdy1998cdy@163.com>
2023-05-07 19:59:18 +0800

    初次部署

--- packages/blog/docs/.vitepress/config.ts
+++ packages/blog/docs/.vitepress/config.ts
@@ -37,52 +37,52 @@ export default defineConfig({
     footer: {
       message: '如有转载或 CV 的请标注本站原文地址',
       copyright: 'Copyright © 2019-present maomao'
-    },
+    }
 
     /* Algolia DocSearch 配置 */
-    algolia: {
-      appId: 'UVB82VCF2Y',
-      apiKey: '4c67197afadb283c77e084bd50618c60',
-      indexName: 'fe-mm',
-      placeholder: '搜索',
-      translations: {
-        button: {
-          buttonText: '搜索',
-          buttonAriaLabel: '搜索'
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
-          },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存至搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索提供者'
-          },
-          noResultsScreen: {
-            noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为该查询应该有结果？',
-            reportMissingResultsLinkText: '点击反馈'
-          }
-        }
-      }
-    }
+    // algolia: {
+    //   appId: 'UVB82VCF2Y',
+    //   apiKey: '4c67197afadb283c77e084bd50618c60',
+    //   indexName: 'fe-mm',
+    //   placeholder: '搜索',
+    //   translations: {
+    //     button: {
+    //       buttonText: '搜索',
+    //       buttonAriaLabel: '搜索'
+    //     },
+    //     modal: {
+    //       searchBox: {
+    //         resetButtonTitle: '清除查询条件',
+    //         resetButtonAriaLabel: '清除查询条件',
+    //         cancelButtonText: '取消',
+    //         cancelButtonAriaLabel: '取消'
+    //       },
+    //       startScreen: {
+    //         recentSearchesTitle: '搜索历史',
+    //         noRecentSearchesText: '没有搜索历史',
+    //         saveRecentSearchButtonTitle: '保存至搜索历史',
+    //         removeRecentSearchButtonTitle: '从搜索历史中移除',
+    //         favoriteSearchesTitle: '收藏',
+    //         removeFavoriteSearchButtonTitle: '从收藏中移除'
+    //       },
+    //       errorScreen: {
+    //         titleText: '无法获取结果',
+    //         helpText: '你可能需要检查你的网络连接'
+    //       },
+    //       footer: {
+    //         selectText: '选择',
+    //         navigateText: '切换',
+    //         closeText: '关闭',
+    //         searchByText: '搜索提供者'
+    //       },
+    //       noResultsScreen: {
+    //         noResultsText: '无法找到相关结果',
+    //         suggestedQueryText: '你可以尝试查询',
+    //         reportMissingResultsText: '你认为该查询应该有结果？',
+    //         reportMissingResultsLinkText: '点击反馈'
+    //       }
+    //     }
+    //   }
+    // }
   }
 })
--- packages/blog/docs/.vitepress/configs/sidebar.ts
+++ packages/blog/docs/.vitepress/configs/sidebar.ts
@@ -7,13 +7,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: 'npm 踩坑记录', link: '/pit/npm' },
         { text: 'PC 踩坑记录', link: '/pit/pc' },
-        {
-          text: 'H5 踩坑记录',
-          items: [
-            { text: 'npm 踩坑记录', link: '/pit/npm' },
-            { text: 'PC 踩坑记录', link: '/pit/pc' }
-          ]
-        }
+        { text: 'H5 踩坑记录', link: '/pit/h5' }
       ]
     }
   ]
