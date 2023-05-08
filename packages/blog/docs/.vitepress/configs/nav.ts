32abcd37f995671f6bc2f1b024cbf7e5a4d2fcd1
左右 <cdy1998cdy@163.com>
2023-05-08 18:04:27 +0800

    补充博客笔记，还需要补充完sidebar

--- packages/blog/docs/.vitepress/configs/nav.ts
+++ packages/blog/docs/.vitepress/configs/nav.ts
@@ -2,7 +2,56 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   {
-    text: '踩坑记录',
+    text: '键入佳境',
+    items: [
+      { text: 'HTML&CSS', link: '/fe/html&css/html' },
+      { text: 'Javascript', link: '/fe/javascript/base/func' },
+      { text: 'TypeScript', link: '/fe/typescript/base' }
+    ],
+    activeMatch: '^/fe'
+  },
+  {
+    text: '源码学习',
+    link: '/analysis/react/18',
+    activeMatch: '^/analysis'
+  },
+  {
+    text: 'Framework',
+    items: [
+      {
+        text: '移动端',
+        items: [
+          { text: '微信小程序', link: '/framework/applet/' },
+          { text: 'uniapp', link: '/framework/applet/uniapp' },
+          { text: 'React Native', link: '/framework/mobile/react-native' }
+        ]
+      },
+      {
+        text: 'react',
+        items: [
+          { text: 'React', link: '/framework/react/' },
+          { text: 'React Redux', link: '/framework/react/redux' }
+        ]
+      },
+      {
+        text: 'vue',
+        items: [
+          { text: 'Vue2', link: '/framework/vue/' },
+          { text: 'Vue3', link: '/framework/vue/vue3' },
+          { text: 'Vuex', link: '/framework/vue/vuex' }
+        ]
+      }
+    ],
+    activeMatch: '^/framework'
+  },
+  { text: 'Node', link: '/node/', activeMatch: '^/node' },
+  {
+    text: 'Workflow',
+    items: [{ text: '开发规范', link: '/workflow/develop-spec' }],
+    activeMatch: '^/workflow'
+  },
+  {
+    text: '踩坑日记',
     link: '/pit/npm',
     activeMatch: '^/pit'
   }
