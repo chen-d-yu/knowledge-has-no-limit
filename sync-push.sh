c64414287f0fb4ac6ceb17049f9548fcd852a2e8
zuoyou <cdy1998cdy@163.com>
2023-05-13 20:05:35 +0800

    测试脚本3

--- sync-push.sh
+++ sync-push.sh
@@ -31,3 +31,5 @@ git push -f https://gitee.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pag
 git push -f https://github.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
 
 cd -
+
+rm -f packages/blog/.vitepress/dist
