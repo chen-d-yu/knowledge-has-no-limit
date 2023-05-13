060362137e46c90c2fe2b09c4b42a13a0a1bf7ea
zuoyou <cdy1998cdy@163.com>
2023-05-13 20:00:19 +0800

    测试脚本2

--- sync-push.sh
+++ sync-push.sh
@@ -27,7 +27,7 @@ git add -A
 git commit -m 'deploy'
 
 # 发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
 git push -f https://gitee.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
+git push -f https://github.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
 
 cd -
