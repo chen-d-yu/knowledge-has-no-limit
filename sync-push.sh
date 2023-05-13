8238ab88f8316d777f1ae0fdc05336e4776e95ad
zuoyou <cdy1998cdy@163.com>
2023-05-13 19:48:17 +0800

    试一下部署脚本

--- sync-push.sh
+++ sync-push.sh
@@ -10,3 +10,24 @@ git commit -m $1
 
 git push origin master
 git push github master
+
+# 进入博客仓库
+cd packages/blog
+
+# 生成静态文件
+pnpm run build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+cp -r ../../../../../.github ./
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
+git push -f https://gitee.com/chen-d-yu/knowledge-has-no-limit.git master:gh-pages
+
+cd -
