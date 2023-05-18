533a6409b32272280d8a1f369d81ad555bc43869
zuoyou <cdy1998cdy@163.com>
2023-05-18 23:17:51 +0800

    npm

--- packages/system-learning/part-6-包管理工具/01_概述.js
+++ packages/system-learning/part-6-包管理工具/01_概述.js
@@ -3,9 +3,9 @@ https://juejin.cn/post/7179122320660201532
 
 
 
+npm init -y 生成package.json
 
-
-
+npm install packageName
 
 
 */
---
+++ packages/system-learning/part-6-包管理工具/02_package的配置.js
@@ -0,0 +1,18 @@
+/*
+常见的属性
+name: 项目名称
+version：项目版本
+private：私有项目，不会出现意外发布的情况
+
+
+https://notes.fe-mm.com/fe/node/pkg#scripts
+
+
+
+
+
+
+
+
+
+*/
